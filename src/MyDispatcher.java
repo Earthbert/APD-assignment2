1547cbc509f1be7a4f4994c0b4411e0e167773b7
Earthbert <daraban_timotei@tutanota.com>
2023-11-26 13:19:45 +0200

    Add initial implementation

--- src/MyDispatcher.java
+++ src/MyDispatcher.java
@@ -1,8 +1,10 @@
 /* Implement this class. */
 
+import java.util.Comparator;
 import java.util.List;
 
 public class MyDispatcher extends Dispatcher {
+    private Integer lastHostIndex = 0;
 
     public MyDispatcher(SchedulingAlgorithm algorithm, List<Host> hosts) {
         super(algorithm, hosts);
@@ -10,5 +12,51 @@ public class MyDispatcher extends Dispatcher {
 
     @Override
     public void addTask(Task task) {
+        switch (algorithm) {
+            case ROUND_ROBIN:
+                addTask_RR(task);
+                break;
+            case LEAST_WORK_LEFT:
+                addTask_LWL(task);
+                break;
+            case SHORTEST_QUEUE:
+                addTask_SQ(task);
+                break;
+            case SIZE_INTERVAL_TASK_ASSIGNMENT:
+                addTask_SITA(task);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void addTask_RR(Task task) {
+        hosts.get(lastHostIndex).addTask(task);
+        lastHostIndex = (lastHostIndex + 1) % hosts.size();
+    }
+
+    private void addTask_LWL(Task task) {
+        hosts.stream().min(Comparator.comparing(Host::getWorkLeft)).get().addTask(task);
+    }
+
+    private void addTask_SQ(Task task) {
+        hosts.stream().min(Comparator.comparing(Host::getQueueSize)).get().addTask(task);
+    }
+
+    private void addTask_SITA(Task task) {
+        switch (task.getType()) {
+            case SHORT:
+                hosts.get(0).addTask(task);
+                break;
+            case LONG:
+                hosts.get(1).addTask(task);
+                break;
+            case MEDIUM:
+                hosts.get(2).addTask(task);
+                break;
+            default:
+                break;
+    
+        }
     }
 }
--- src/MyHost.java
+++ src/MyHost.java
@@ -1,25 +1,79 @@
 /* Implement this class. */
 
+import java.util.Comparator;
+import java.util.concurrent.PriorityBlockingQueue;
+
 public class MyHost extends Host {
+    private PriorityBlockingQueue<Task> queue = new PriorityBlockingQueue<>(10, Comparator.comparing(Task::getPriority));
+    private Task runningTask = null;
+
+    private boolean isShutdown = false;
+
     @Override
     public void run() {
+        while (true) {
+            if (isShutdown) {
+                return;
+            }
+            if (runningTask == null) {
+                try {
+                    this.wait();
+                } catch (InterruptedException e) {
+                    System.exit(1);
+                    e.printStackTrace();
+                }
+                runningTask = queue.poll();
+            } else {
+                runTask(runningTask);
+            }
+        }
     }
 
     @Override
     public void addTask(Task task) {
+        if (isShutdown) {
+            return;
+        }
+        queue.add(task);
+        this.notify();
     }
 
     @Override
     public int getQueueSize() {
-        return 0;
+        return runningTask == null ? queue.size() : queue.size() + 1; 
     }
 
     @Override
     public long getWorkLeft() {
-        return 0;
+        return queue.stream().mapToLong(Task::getLeft).sum();
     }
 
     @Override
     public void shutdown() {
+        queue.clear();
+        this.notify();
+        isShutdown = true;
+    }
+
+    private void runTask(Task task) {
+        long currentTime = System.currentTimeMillis();
+
+        try {
+            this.wait(task.getLeft(), 0);
+        } catch (InterruptedException e) {
+            System.exit(1);
+            e.printStackTrace();
+        }
+
+        long elapsedTime = System.currentTimeMillis() - currentTime;
+
+        task.setLeft(task.getLeft() - elapsedTime);
+
+        if (task.getLeft() <= 0) {
+            task.finish();
+            runningTask = queue.poll();
+            return;
+        } else
+            queue.add(runningTask);
     }
 }
